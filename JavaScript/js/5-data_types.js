2e2f58a60280d8646474e0e30e8d04ab13520e64
ryjkowna <ryjkowna@gmail.com>
2018-12-01 12:48:12 +0100

    typy zmiennych

--- JavaScript/js/5-data_types.js
+++ JavaScript/js/5-data_types.js
@@ -1,1 +1,19 @@
-console.log("Magdalena")
+let imie = 3;
+
+let PI = 3.14;
+let liczba = 308;
+let wyplata = 15000;
+
+console.log(typeof wyplata);
+
+let stringNapis = "To są stringi";
+console.log(typeof stringNapis);
+
+//-- typ logiczny
+
+let prawdaCzyFalsz = true;
+console.log(typeof prawdaCzyFalsz);
+
+//-- 
+let czyNull =( );
+console.log(typeof czyNull)
