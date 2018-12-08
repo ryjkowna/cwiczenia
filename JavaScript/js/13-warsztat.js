094aa537bd0fa60130ceb0c58cdc1941e7a44c42
ryjkowna <ryjkowna@gmail.com>
2018-12-08 09:21:46 +0100

    powtorka

---
+++ JavaScript/js/13-warsztat.js
@@ -0,0 +1,11 @@
+
+
+let trzy = 4;
+const mnozymy=() =>{
+let trzy = 3;
+let szesc = 6;
+let siedem = 7;
+let wartosc = trzy * szesc * siedem;
+console.log(wartosc);}
+console.log(trzy);
+mnozymy();
